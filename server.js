5b07c2c7f13b866c0c134713607f5355f8197050
alang66 <alang.6@gmail.com>
2017-01-09 08:57:20 +0800

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ if(args[0].indexOf('@') > -1)
 
 
 appid   =   args[2],
-images  =   ["malaohu/ssr-with-net-speeder","lowid/ss-with-net-speeder","smounives/shadowsocksr-docker","mritd/shadowsocks"];
+images  =   ["malaohu/ssr-with-net-speeder","lowid/ss-with-net-speeder","smounives/shadowsocksr-docker","mritd/shadowsocks","tommylau/shadowsocks"];
 
 
 app.get('/', function(req, res) {
